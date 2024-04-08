4bbb56e085e85ec1795d743a6143c74e09803bc8
Azyxon <74427931+Azyxon@users.noreply.github.com>
2024-04-08 20:23:34 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -15,7 +15,7 @@ local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
-ScreenGui.Parent = CoreGui;
+ScreenGui.Parent = LocalPlayer.PlayerGui;
 
 local Toggles = {};
 local Options = {};
