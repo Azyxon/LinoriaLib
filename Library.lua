dd6fd36fe52a64dd0ce6085f88e4c45e6c9012f2
Azyxon <74427931+Azyxon@users.noreply.github.com>
2025-01-04 22:38:39 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3539,7 +3539,7 @@ function Library:CreateWindow(...)
         if Toggled then
             -- A bit scuffed, but if we're going from not toggled -> toggled we want to show the frame immediately so that the fade is visible.
             Outer.Visible = true;
-
+            --[[
             task.spawn(function()
                 -- TODO: add cursor fade?
                 local State = InputService.MouseIconEnabled;
@@ -3578,6 +3578,7 @@ function Library:CreateWindow(...)
                 Cursor:Remove();
                 CursorOutline:Remove();
             end);
+            ]]
         end;
 
         for _, Desc in next, Outer:GetDescendants() do
