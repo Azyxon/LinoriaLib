6580319a9364b5a79a8cf0f14355c1567cdd255a
Reverbgamer <87449746+Reverbgamer@users.noreply.github.com>
2026-04-04 20:51:11 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1489,8 +1489,8 @@ do
             ProcessButtonParams('SubButton', SubButton, ...)
             self.Outer.Size = UDim2.new(0.5, -2, 0, 20)
             SubButton.Outer, SubButton.Inner, SubButton.Label = CreateBaseButton(SubButton)
-            SubButton.Outer.Position = UDim2.new(0.5, 2, 0, 0)
-            SubButton.Outer.Size = UDim2.new(0.5, -4, 1, 0)
+            SubButton.Outer.Position = UDim2.new(1, 3, 0, 0)
+            SubButton.Outer.Size = UDim2.fromOffset(self.Outer.AbsoluteSize.X - 2, self.Outer.AbsoluteSize.Y)
             SubButton.Outer.Parent = self.Outer
 
             function SubButton:AddTooltip(tooltip)
