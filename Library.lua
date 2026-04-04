939ed3cd98d065e4accd70bb24dd24791b189b6d
Reverbgamer <87449746+Reverbgamer@users.noreply.github.com>
2026-04-04 20:44:10 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1489,7 +1489,7 @@ do
             ProcessButtonParams('SubButton', SubButton, ...)
             self.Outer.Size = UDim2.new(0.5, -2, 0, 20)
             SubButton.Outer, SubButton.Inner, SubButton.Label = CreateBaseButton(SubButton)
-            SubButton.Outer.Position = UDim2.new(1, 3, 0, 0)
+            SubButton.Outer.Position = UDim2.new(0.5, 0, 0, 0)
             SubButton.Outer.Size = UDim2.new(1, 0, 1, 0)
             SubButton.Outer.Parent = self.Outer
 
