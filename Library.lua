bb157e4073bea1547df297947a583a2360897095
Reverbgamer <87449746+Reverbgamer@users.noreply.github.com>
2026-06-28 19:38:38 +0200

    test

--- Library.lua
+++ Library.lua
@@ -2115,7 +2115,7 @@ do
 
     function Funcs:AddSlider(Idx, Info)
         assert(Info.Default, 'AddSlider: Missing default value.');
-        assert(Info.Text, 'AddSlider: Missing slider text.');
+        -- assert(Info.Text, 'AddSlider: Missing slider text.');
         assert(Info.Min, 'AddSlider: Missing minimum value.');
         assert(Info.Max, 'AddSlider: Missing maximum value.');
         assert(Info.Rounding, 'AddSlider: Missing rounding value.');
@@ -2133,7 +2133,7 @@ do
         local Groupbox = self;
         local Container = Groupbox.Container;
 
-        if not Info.Compact then
+        if Text and not Info.Compact then
             Library:CreateLabel({
                 Size = UDim2.new(1, 0, 0, 10);
                 TextSize = 14;
